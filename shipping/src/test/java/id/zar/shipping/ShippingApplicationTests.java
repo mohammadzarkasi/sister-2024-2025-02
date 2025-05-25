f54e5f3e79f5209f2b8d2018aebc6da395b0c2d8
mozar <mozar@mozars-MacBook-Pro.local>
2025-05-25 17:09:34 +0700

    init project: account, shipping, inventory

---
+++ shipping/src/test/java/id/zar/shipping/ShippingApplicationTests.java
@@ -0,0 +1,13 @@
+package id.zar.shipping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

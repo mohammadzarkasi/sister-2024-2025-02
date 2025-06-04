2ce27c632f0de3e327f84b490370f863560046cd
mozar <mozar@mozars-MacBook-Pro.local>
2025-06-04 07:02:47 +0700

    buat modul eureka server

---
+++ eureka/src/test/java/id/zar/discovery/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package id.zar.discovery.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
